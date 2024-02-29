948171e1c0f2d514df9522dfecf1b8ddaa1d9a1d
adpop <adpop@debian-11.8.0-beaglebone.myguest.virtualbox.org>
2024-02-28 17:09:40 -0800

    add: terminalOutput line 2

--- app/src/terminalOutput.c
+++ app/src/terminalOutput.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <time.h>
+#include <string.h>
 #include "terminalOutput.h"
 #include "sampler.h"
 #include "periodTimer.h"
@@ -31,9 +32,28 @@ void* Terminal_startDisplay() {
         int dips = getNumDips();
         //TODO: timing jitter info
         
-        char line1[] = "#Smpl/s = %.3d\tPOT @ %.4d => %.3dHz\tavg = %.3fV\tdips = %.3d\tSmpl ms[ %.3f, %.3f] avg %.3f/%.3d\n\n";
-        printf(line1, sampleNum, potRaw, freq, avgLightLvl, dips);
+        char line1[] = "#Smpl/s = %.3d\tPOT @ %.4d => %.3dHz\tavg = %.3fV\tdips = %.3d\tSmpl ms[ %.3f, %.3f] avg %.3f/%.3d\n";
         
+        char line2[1024];
+        strncpy(line2, "\0", 1);
+        double* doubleHist = Sampler_getHistory(&sampleNum);
+        int histLen = sampleNum;
+        int histInc = 1;
+        if(sampleNum > 20) {
+            histLen = 20;
+            histInc = sampleNum/20;
+        }
+
+        for(int i =0; i < histLen; i++) {
+            float histVal = convertA2D(doubleHist[i*histInc]);
+            char sampleStr[330];
+            snprintf(sampleStr, 330, "%.3d:%.3f  ", i*histInc, histVal);
+            strncat(line2, sampleStr, 330);
+        }
+        strncat(line2, "\n\n\0", 3);
+        printf(line1, sampleNum, potRaw, freq, avgLightLvl, dips);
+        printf(line2);
+        free(doubleHist);
         if(secondAhead - getTimeInMs() > 0) {
            sleepForMs(secondAhead - getTimeInMs()); 
         }
